22c20d31cf787bb26118aab56d0e14798f6ee698
Christophe Lab <ctlabfr@gmail.com>
2025-05-15 18:17:55 +0200

    Github: deploy astro with static adapter on github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import staticAdapter from "@astrojs/static";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    adapter: staticAdapter(),
+    base: '/', 
+    output: 'static'
+  });
