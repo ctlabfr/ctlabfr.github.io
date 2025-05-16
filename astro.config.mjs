7dca92ebd0b407c9f9d3a3b1a4cdf509bad2cfea
Christophe Lab <ctlabfr@gmail.com>
2025-05-16 15:58:44 +0200

    dev - Ajout extension astro-expressive-code pour améliorer le rendu des éléments de code dans les pages html

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import astroExpressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
-    base: '/', 
-    output: 'static'
-  });
+  base: "/",
+  output: "static",
+  integrations: [
+    astroExpressiveCode({
+      themes: ["dracula"],
+      frames: {
+        // Example: Hide the "Copy to clipboard" button
+        showCopyToClipboardButton: true,
+      },
+    }),
+  ],
+});
